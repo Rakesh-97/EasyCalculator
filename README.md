396eaac900b1f0cc83c9de590220fa47122901d1
Spider34 <rakekum34@gmail.com>
2017-08-22 18:49:33 -0800

    Project is Submited

---
+++ README.md
@@ -0,0 +1,1 @@
+This is simple calculator Android App which is work on basic idea of Math's Operation.
---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="90320ead-bae8-4516-b11e-c424c087988c")
+public class DataBindingInfo {}
